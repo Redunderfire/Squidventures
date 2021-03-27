e3faa4fcd9e6f745d7ddc761238b1786def0c32f
Tyler C <tconnelly7440@floridapoly.edu>
2021-03-27 11:37:46 -0400

    Remove GroundChecker Object
    Remove the ground checking object required for ground detection.

--- Assets/Input/PlayerMove.cs
+++ Assets/Input/PlayerMove.cs
@@ -11,7 +11,7 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] public float gravity = -9.81f;
     public float jumpHeight = 3f;
 
-    public Transform groundCheck;
+   // public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
     bool isGrounded, jump;
@@ -21,7 +21,9 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        isGrounded = Physics.CheckSphere(transform.position, groundDistance, groundMask);
+
+        //isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         if(isGrounded && velocity.y < 0){
             velocity.y = -2f;
         }
