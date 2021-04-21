64fdc95a0929257464ec103d197695aba5ad8c7e
Tyler C <tconnelly7440@floridapoly.edu>
2021-04-21 11:00:14 -0400

    Added Animations, Logo
    Added logo animations

--- Assets/Scripts/UI/MenuLogoAnimator.cs
+++ Assets/Scripts/UI/MenuLogoAnimator.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MenuModelAnimator : MonoBehaviour
+public class MenuLogoAnimator : MonoBehaviour
 {
     public AnimationCurve yCurve;
+    public AnimationCurve scaleCurve;
     public float ySpeed = 100.0f;
+    public float scaleSpeed = 1.0f;
+    public Vector3 finalScale;
     private float curveDeltaTime = 0.0f;
+    private float graphScale = 0.0f;
     private Vector3 initialPosition;
-
-
 
     void Start(){
         initialPosition = transform.position;
@@ -21,10 +23,12 @@ public class MenuModelAnimator : MonoBehaviour
         //currentPosition.x += speed * Time.deltaTime;
         // Call evaluate on that time   
         curveDeltaTime+= Time.deltaTime;   
-        currentPosition.y = initialPosition.y+(ySpeed * yCurve.Evaluate(curveDeltaTime)); 
-        // Update the current position
+        currentPosition.y = initialPosition.y+(ySpeed * yCurve.Evaluate(curveDeltaTime));   
+        
+        graphScale = scaleCurve.Evaluate(scaleSpeed*curveDeltaTime);
+        transform.localScale=finalScale*graphScale;
+        // Update the current position\
         transform.position = currentPosition;
         //transform.position = new Vector3(transform.position.x, myCurve.Evaluate((float)(Time.timeAsDouble % myCurve.length)), transform.position.z);
     }
-} 
- 
+}
