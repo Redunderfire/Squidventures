1eb00da89dc6aa7780a76c4f9420196a428c7103
Tyler C <tconnelly7440@floridapoly.edu>
2021-04-21 16:21:51 -0400

    Added basic jetpack functionality
    Added jetpack

--- Assets/Input/PlayerMove.cs
+++ Assets/Input/PlayerMove.cs
@@ -5,44 +5,57 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
 
-    public CharacterController controller;
+    private CharacterController _controller;
+    private float _fuel;
 
     [SerializeField] public float walkingSpeed = 7f;
     [SerializeField] public float runningSpeed = 9.5f;
     [SerializeField] public float gravity = -9.81f;
     public float jumpHeight = 3f;
+    public float maxFuel = 100f;
+    public float fuelReduce = 100f;
+    public float jetpackAcceleration = 1.5f;
+    public float maxJetpackVelocity = 1.5f;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
-    bool isGrounded, jump;
-    float x, z, speed;
-       
-    Vector3 velocity;
+    bool isGrounded, jump, boost;
+    private float x, z, speed;
+    private Vector3 yVelocity = Vector3.zero;
 
     // Sets speed at beginning, consequence of the way Unity handles floats outside of this scope
     private void Start(){
         SprintEvent(false);
+        _controller = this.GetComponent<CharacterController>();
+        _fuel = maxFuel;
     }
     //Update is called once per frame
     private void Update()
     {
-        //Check for ground collision with the player
-        isGrounded = Physics.CheckSphere(transform.position, groundDistance, groundMask);
-
-        //Grounding logic
-        if(isGrounded && velocity.y < 0){
-            velocity.y = -2f;
+        //Ground Checks and velocity adjustment
+        isGrounded =  isGrounded = Physics.CheckSphere(transform.position, groundDistance, groundMask);
+        if (isGrounded && yVelocity.y < 0)
+        {
+            yVelocity.y = 0f;
+        }
+        if (yVelocity.y > maxJetpackVelocity){ //clamp velocity
+            yVelocity.y = maxJetpackVelocity;
         }
 
-        //Jump logic
+       Vector3 move = new Vector3(x, 0, z);
+       move = transform.TransformDirection(move.normalized);
+       _controller.Move(move * Time.deltaTime * speed);
+
         if(jump){
-            velocity.y += Mathf.Sqrt(jumpHeight * -2f * gravity);
+            yVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
             jump = false;
         }
+        if(boost){
+             yVelocity.y += 1.0f * jetpackAcceleration ;
+        }
 
-        velocity.y += gravity * Time.deltaTime; //Update Gravity
-        Vector3 move = transform.right * x + transform.forward * z; 
-        controller.Move(move * speed * Time.deltaTime); //Apply horizontal movement
-        controller.Move(velocity * Time.deltaTime); //Apply vertical velocity
+        yVelocity.y += gravity * Time.deltaTime;
+        _controller.Move(yVelocity * Time.deltaTime);
+
     }
 
     //Receive input from InputManager
@@ -52,13 +65,26 @@ public class PlayerMove : MonoBehaviour
     }
 
     //Set jump
-    public void JumpEvent() {
-        if(isGrounded){
-            jump = true;
-        } else {
-            jump = false;
+
+
+    public void JumpEvent(bool pressed) {
+        if(pressed){
+            if(isGrounded){
+                jump = true;
+                boost = false;
+                Debug.Log("jump event logged");  
+            } else {
+                jump = false;
+                boost = true;
+                Debug.Log("boost event logged");
+            }
         }
-    }
+        if(!pressed){
+            Debug.Log("Jump events canceled");
+                jump = false;
+                boost = false;
+        }
+    } 
 
     //Set speed
     public void SprintEvent(bool sprinting){
