7bdd3f0189eff6dd5c281880cbc27da05091ee37
Tyler C <tconnelly7440@floridapoly.edu>
2021-04-22 09:30:36 -0400

    Added Objective Controls, spaceship model

--- Assets/Scripts/Player/ObjectiveManager.cs
+++ Assets/Scripts/Player/ObjectiveManager.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 //This will need to be expanded as time goes on to account for ending the game and sending informtion to the HUD.
 public class ObjectiveManager : MonoBehaviour
 {
-    [SerializeField] public int maxValue = 6;
+    [SerializeField]private int maxValue = 6;
     private int currentValue;
+
+    public ObjectiveCircle objectiveCircle;
 
     // Start is called before the first frame update
     void Start() {
         currentValue = 0;
+        objectiveCircle.setMaxObj(maxValue);
+        objectiveCircle.setObj(0);
+
     }
 
     // Increment our objective's value
     public void incrementObjValue(){
         currentValue++;
+        objectiveCircle.setObj(currentValue);
         if(currentValue >= maxValue){ // if all objectives collected, call the endgame function
             endGame();
         }
